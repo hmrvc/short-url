db09745939f31e46d59720b4393f4183b0de8431
hmrvc <hmrvc@yahoo.com.tw>
2021-12-13 10:24:44 +0800

    Import mongoose connect with database

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 const port = 3000
+
+mongoose.connect('mongodb://localhost/short-url')
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongdb connect!')
+})
 
 app.get('/', (req, res) => {
   res.send(`<h1>Hi</h1>`)
