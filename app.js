55b176eb233a1656587844283106af0867921a78
hmrvc <hmrvc@yahoo.com.tw>
2021-12-14 10:33:36 +0800

    Add random function in app.js

--- app.js
+++ app.js
@@ -26,6 +26,18 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   const url = req.body.url
   console.log(url)
+//產生五碼亂數字元
+  const CHARLIST = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  const CHARNUM = CHARLIST.length
+  const TARGETNUM = 5
+
+  let result = ''
+  for (let i = 0; i < TARGETNUM; i++) {
+    let choosenIndex = Math.floor(Math.random() * CHARNUM)
+    let choosenChar = CHARLIST[choosenIndex]
+    result += choosenChar
+  } 
+  console.log(result)
 })
 
 app.listen(port, () => {
